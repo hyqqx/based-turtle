5e635a919582c8144564af2270476a747fb85c0c
maksswell <maks.hyinya22@gmail.com>
2026-07-05 21:45:37 +0300

    Include base app ID in layout.tsx
    Add base app ID to layout configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export async function generateMetadata(): Promise<Metadata> {
     title: farcasterConfig.miniapp.name,
     description: farcasterConfig.miniapp.description,
     other: {
+      "base:app_id": "6a4aa450e1a7f31c66ea452e",
       "fc:frame": JSON.stringify({
         version: farcasterConfig.miniapp.version,
         imageUrl: farcasterConfig.miniapp.heroImageUrl,
